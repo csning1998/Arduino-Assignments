bd5650b9435df40fad7477184b392e72aff945bb
ChingSan Ning <january20070120@gmail.com>
2024-10-19 22:41:38 +0800

    Fixed: I2C Declaration and the logical structure of the code.

--- Assignment2-LCD_LED_Buzzer-Control/LCD_LED_Buzzer-Control-Better_Approach/LCD_LED_Buzzer-Control-Better_Approach.ino
+++ Assignment2-LCD_LED_Buzzer-Control/LCD_LED_Buzzer-Control-Better_Approach/LCD_LED_Buzzer-Control-Better_Approach.ino
@@ -1,24 +1,30 @@
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-// Define the Pin Node
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal_I2C lcd(0x26, 16, 2);
 
-const int redLED = 6;
-const int yellowLED = 7;
-const int greenLED = 8;
-const int buzzer = 9;
+const int RED_LED = 6;
+const int YELLOW_LED = 7;
+const int GREEN_LED = 8;
+const int BUZZER = 9;
+
+const int BUZZER_FREQ = 1000;
+const int BUZZER_DURATION = 200;
+const int BUZZER_REPEAT = 5;
+const int YELLOW_INTERVAL = 1000;
+const int GREEN_INTERVAL = 3000;
 
 void setup() {
-  // Initialize the LED and Buzzer to be ready for O/P
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-
-  lcd.begin(16, 2);
-  lcd.clear();
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 
   Serial.begin(9600);
+  lcd.init();					
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Awaiting Input...");
 }
 
 void loop() {
@@ -31,63 +37,78 @@ void loop() {
 
 /* We deal with the functionalities of the program below */
 
-void outputInitializer() {
+void resetOutputs() {
   lcd.clear();
-  digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(greenLED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
   buzzerOff();
 }
 
 void buzzerOff() {
-  noTone(buzzer);
+  noTone(BUZZER);
 }
 
-void ledControl(int ledPin, bool state, const char* msg) {
+void ledControl(int ledPin, bool state) {
   digitalWrite(ledPin, state ? HIGH : LOW);
+}
+
+void displayMessage(const char* msg) {
+  lcd.clear();
   lcd.print(msg);
 }
 
-void buzzerControl(int freq, int duration, int repeat, int interval) {
+void buzzerController(int freq, int duration, int repeat, int interval) {
+  Serial.println("Start Buzzing");
+  // Serial.print("num of freq: "); Serial.println(freq);
+  // Serial.print("num of duration: "); Serial.println(duration);
+  // Serial.print("num of repeat: "); Serial.println(repeat);
+  // Serial.print("num of interval: "); Serial.println(interval);
+  
   for (int i = 0; i < repeat; i++) {
-    tone(buzzer, freq, duration);
+    tone(BUZZER, freq, duration);
     delay(interval);
   }
-  noTone(buzzer);
+  buzzerOff();
+}
+
+void ledController(int ledPin, const char* msg, int repeat, int interval) {
+  ledControl(ledPin, true);
+  displayMessage(msg);
+  
+  Serial.println("Start Lightening");
+  // Serial.print("LED PIN is: "); Serial.println(ledPin);
+  // Serial.print("num of repeat: "); Serial.println(repeat);
+  // Serial.print("num of interval: "); Serial.println(interval);
+  
+  if (repeat > 0 && interval > 0) {
+    buzzerController(BUZZER_FREQ, BUZZER_DURATION, repeat, interval);
+  }
 }
 
 void inputHandler(char input) {
-  outputInitializer();
-
+  resetOutputs();
+  Serial.println("=== Session Started ===");
+  // Serial.print("BUZZER FREQ is: "); Serial.println(BUZZER_FREQ);
+  // Serial.print("BUZZER REPEAT is: "); Serial.println(BUZZER_REPEAT);
+  // Serial.print("GREEN INTERVAL is: "); Serial.println(GREEN_INTERVAL);
+  // Serial.print("YELLOW INTERVAL is: "); Serial.println(YELLOW_INTERVAL);
+  
   switch (input) {
     case 'C':
       break;
     case 'R':
-      redLEDHandler();
+      ledController(RED_LED, "RED ON", 0, 0);
       break;
     case 'Y':
-      yellowLEDHandler();
+      ledController(YELLOW_LED, "YELLOW ON", BUZZER_REPEAT, YELLOW_INTERVAL);
       break;
     case 'G':
-      greenLEDHandler();
+      ledController(GREEN_LED, "GREEN ON", BUZZER_REPEAT, GREEN_INTERVAL);
       break;
     default:
-      lcd.print("(422) Unprocessable Entity");
+      displayMessage("(422) Unprocessable Entity");
+      Serial.println("Invalid command received.");
       break;
   }
-}
-
-void redLEDHandler() {
-  ledControl(redLED, true, "RED ON");
-  tone(buzzer, 1000);
-}
-
-void yellowLEDHandler() {
-  ledControl(yellowLED, true, "YELLOW ON");
-  buzzerControl(1000, 200, 5, 1000);
-}
-
-void greenLEDHandler() {
-  ledControl(greenLED, true, "GREEN ON");
-  buzzerControl(1000, 200, 3, 3000);
 }
