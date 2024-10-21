b1d43cc89823ebc97be795f28ae3aa1484e1973d
ChingSan Ning <january20070120@gmail.com>
2024-10-21 23:36:22 +0800

    Fixed: Logical error on processing continuous siren with Red LED and buzzer

--- Assignment2-LCD_LED_Buzzer-Control/LCD_LED_Buzzer-Control-Better_Approach/LCD_LED_Buzzer-Control-Better_Approach.ino
+++ Assignment2-LCD_LED_Buzzer-Control/LCD_LED_Buzzer-Control-Better_Approach/LCD_LED_Buzzer-Control-Better_Approach.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x26, 16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int RED_LED = 6;
 const int YELLOW_LED = 7;
@@ -14,6 +14,8 @@ const int BUZZER_REPEAT = 5;
 const int YELLOW_INTERVAL = 1000;
 const int GREEN_INTERVAL = 3000;
 
+volatile bool keepBuzzing = false;
+
 void setup() {
   pinMode(RED_LED, OUTPUT);
   pinMode(YELLOW_LED, OUTPUT);
@@ -21,7 +23,7 @@ void setup() {
   pinMode(BUZZER, OUTPUT);
 
   Serial.begin(9600);
-  lcd.init();					
+  lcd.init();          
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Awaiting Input...");
@@ -30,8 +32,14 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     char input = Serial.read();
-    input = toupper(input);
+    if (input == '\n' || input == '\r') {
+      return;
+    }
     inputHandler(input);
+  }
+
+  if (keepBuzzing) {
+    tone(BUZZER, BUZZER_FREQ);
   }
 }
 
@@ -42,6 +50,7 @@ void resetOutputs() {
   digitalWrite(RED_LED, LOW);
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
+  keepBuzzing = false;
   buzzerOff();
 }
 
@@ -58,15 +67,22 @@ void displayMessage(const char* msg) {
   lcd.print(msg);
 }
 
+void initialMessage(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Awaiting Input...");
+}
+
 void buzzerController(int freq, int duration, int repeat, int interval) {
   Serial.println("Start Buzzing");
-  // Serial.print("num of freq: "); Serial.println(freq);
-  // Serial.print("num of duration: "); Serial.println(duration);
-  // Serial.print("num of repeat: "); Serial.println(repeat);
-  // Serial.print("num of interval: "); Serial.println(interval);
+  Serial.print("num of freq: "); Serial.println(freq);
+  Serial.print("num of duration: "); Serial.println(duration);
+  Serial.print("num of repeat: "); Serial.println(repeat);
+  Serial.print("num of interval: "); Serial.println(interval);
   
   for (int i = 0; i < repeat; i++) {
     tone(BUZZER, freq, duration);
+    delay(duration);
     delay(interval);
   }
   buzzerOff();
@@ -77,38 +93,43 @@ void ledController(int ledPin, const char* msg, int repeat, int interval) {
   displayMessage(msg);
   
   Serial.println("Start Lightening");
-  // Serial.print("LED PIN is: "); Serial.println(ledPin);
-  // Serial.print("num of repeat: "); Serial.println(repeat);
-  // Serial.print("num of interval: "); Serial.println(interval);
+  Serial.print("LED PIN is: "); Serial.println(ledPin);
+  Serial.print("num of repeat: "); Serial.println(repeat);
+  Serial.print("num of interval: "); Serial.println(interval);
   
   if (repeat > 0 && interval > 0) {
     buzzerController(BUZZER_FREQ, BUZZER_DURATION, repeat, interval);
+  } else if (repeat == -1) {
+    keepBuzzing = true;
+  } else {
+    Serial.println("Invalid parameter(s) received.");
   }
 }
 
 void inputHandler(char input) {
   resetOutputs();
+  input = toupper(input);
   Serial.println("=== Session Started ===");
-  // Serial.print("BUZZER FREQ is: "); Serial.println(BUZZER_FREQ);
-  // Serial.print("BUZZER REPEAT is: "); Serial.println(BUZZER_REPEAT);
-  // Serial.print("GREEN INTERVAL is: "); Serial.println(GREEN_INTERVAL);
-  // Serial.print("YELLOW INTERVAL is: "); Serial.println(YELLOW_INTERVAL);
   
   switch (input) {
     case 'C':
       break;
     case 'R':
-      ledController(RED_LED, "RED ON", 0, 0);
+      ledController(RED_LED, "RED ON", -1, -1);
       break;
     case 'Y':
       ledController(YELLOW_LED, "YELLOW ON", BUZZER_REPEAT, YELLOW_INTERVAL);
+      initialMessage();
       break;
     case 'G':
       ledController(GREEN_LED, "GREEN ON", BUZZER_REPEAT, GREEN_INTERVAL);
+      initialMessage();
       break;
     default:
       displayMessage("(422) Unprocessable Entity");
       Serial.println("Invalid command received.");
+      delay(3000);
+      initialMessage();
       break;
   }
 }
